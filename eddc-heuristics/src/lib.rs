7608c0e83441acc7ebc5be744eb045200c9fab15
Ricky-pon <ricky.quiken@gmail.com>
2022-09-13 10:01:09 +0200

    add main.rs

--- eddc-heuristics/src/lib.rs
+++ eddc-heuristics/src/lib.rs
@@ -1,37 +1,44 @@
 pub struct EDDCParameters {
-    match_score: Vec<Vec<i64>>,
+    match_score: Vec<Vec<f64>>,
     // Insertion to the *unit*. Should be negative.
-    ins_score: Vec<i64>,
+    ins_score: Vec<f64>,
     // Deletion from the *unit*. Should be negative.
-    del_score: Vec<i64>,
+    del_score: Vec<f64>,
 }
 
-fn alignment_dp(s: &Vec<usize>, t: &Vec<usize>, params: &EDDCParameters) -> i64 {
+fn f64_min(a: f64, b: f64) -> f64 {
+    match a <= b {
+        true => a,
+        _ => b,
+    }
+}
+
+fn alignment_dp(s: &Vec<usize>, t: &Vec<usize>, params: &EDDCParameters) -> f64 {
     let n = s.len();
     let m = t.len();
-    let mut dp: Vec<Vec<Option<i64>>> = vec![vec![None; m + 1]; n + 1];
+    let mut dp: Vec<Vec<Option<f64>>> = vec![vec![None; m + 1]; n + 1];
 
-    dp[0][0] = Some(0);
+    dp[0][0] = Some(0.0);
     for i in 0..n + 1 {
         for j in 0..m + 1 {
             if i + 1 <= n {
                 let tmp_val = dp[i][j].unwrap() + params.del_score[s[i]];
                 dp[i + 1][j] = match dp[i + 1][j] {
-                    Some(val) => Some(std::cmp::min(val, tmp_val)),
+                    Some(val) => Some(f64_min(val, tmp_val)),
                     None => Some(tmp_val),
                 }
             }
             if j + 1 <= m {
                 let tmp_val = dp[i][j].unwrap() + params.ins_score[t[j]];
                 dp[i][j + 1] = match dp[i][j + 1] {
-                    Some(val) => Some(std::cmp::min(val, tmp_val)),
+                    Some(val) => Some(f64_min(val, tmp_val)),
                     None => Some(tmp_val),
                 }
             }
             if i + 1 <= n && j + 1 <= m {
                 let tmp_val = dp[i][j].unwrap() + params.match_score[s[i]][t[j]];
                 dp[i + 1][j + 1] = match dp[i + 1][j + 1] {
-                    Some(val) => Some(std::cmp::min(val, tmp_val)),
+                    Some(val) => Some(f64_min(val, tmp_val)),
                     None => Some(tmp_val),
                 }
             }
@@ -41,32 +48,32 @@ fn alignment_dp(s: &Vec<usize>, t: &Vec<usize>, params: &EDDCParameters) -> i64 
     dp[n][m].unwrap()
 }
 
-fn unit_match_score(u: &Vec<char>, v: &Vec<char>) -> i64 {
+fn unit_match_score(u: &[u8], v: &[u8]) -> f64 {
     let n = u.len();
     let m = v.len();
-    let mut dp: Vec<Vec<Option<i64>>> = vec![vec![None; m + 1]; n + 1];
+    let mut dp: Vec<Vec<Option<f64>>> = vec![vec![None; m + 1]; n + 1];
 
-    dp[0][0] = Some(0);
+    dp[0][0] = Some(0.0);
     for i in 0..n + 1 {
         for j in 0..m + 1 {
             if i + 1 <= n {
-                let tmp_val = dp[i][j].unwrap() + 2;
+                let tmp_val = dp[i][j].unwrap() + 1.0;
                 dp[i + 1][j] = match dp[i + 1][j] {
-                    Some(val) => Some(std::cmp::min(val, tmp_val)),
+                    Some(val) => Some(f64_min(val, tmp_val)),
                     None => Some(tmp_val),
                 }
             }
             if j + 1 <= m {
-                let tmp_val = dp[i][j].unwrap() + 2;
+                let tmp_val = dp[i][j].unwrap() + 1.0;
                 dp[i][j + 1] = match dp[i][j + 1] {
-                    Some(val) => Some(std::cmp::min(val, tmp_val)),
+                    Some(val) => Some(f64_min(val, tmp_val)),
                     None => Some(tmp_val),
                 }
             }
             if i + 1 <= n && j + 1 <= m {
-                let tmp_val = dp[i][j].unwrap() + (if u[i] != v[j] { 2 } else { 0 });
+                let tmp_val = dp[i][j].unwrap() + (if u[i] != v[j] { 1.0 } else { 0.0 });
                 dp[i + 1][j + 1] = match dp[i + 1][j + 1] {
-                    Some(val) => Some(std::cmp::min(val, tmp_val)),
+                    Some(val) => Some(f64_min(val, tmp_val)),
                     None => Some(tmp_val),
                 }
             }
@@ -76,18 +83,18 @@ fn unit_match_score(u: &Vec<char>, v: &Vec<char>) -> i64 {
     dp[n][m].unwrap()
 }
 
-fn unit_ins_score(unit: &Vec<char>) -> i64 {
-    unit.len() as i64
+fn unit_ins_score(unit: &[u8]) -> f64 {
+    unit.len() as f64 * 0.5
 }
 
-fn unit_del_score(unit: &Vec<char>) -> i64 {
-    unit.len() as i64
+fn unit_del_score(unit: &[u8]) -> f64 {
+    unit.len() as f64 * 0.5
 }
 
-fn set_params(units: &Vec<Vec<char>>) -> EDDCParameters {
+fn set_params(units: &Vec<&[u8]>) -> EDDCParameters {
     let unit_num = units.len();
 
-    let mut match_score = vec![vec![0; unit_num]; unit_num];
+    let mut match_score = vec![vec![0.0; unit_num]; unit_num];
     for i in 0..unit_num {
         for j in i + 1..unit_num {
             let tmp = unit_match_score(&units[i], &units[j]);
@@ -97,7 +104,7 @@ fn set_params(units: &Vec<Vec<char>>) -> EDDCParameters {
     }
     let ins_score = units.iter().map(|unit| unit_ins_score(unit)).collect();
     let del_score = units.iter().map(|unit| unit_del_score(unit)).collect();
-    println!("{:?}", match_score);
+    // println!("{:?}", match_score);
 
     EDDCParameters {
         match_score,
@@ -106,11 +113,11 @@ fn set_params(units: &Vec<Vec<char>>) -> EDDCParameters {
     }
 }
 
-fn eddc_heuristic(sequences: &Vec<Vec<usize>>, units: &Vec<Vec<char>>) -> Vec<Vec<i64>> {
+pub fn eddc_heuristic(sequences: &Vec<Vec<usize>>, units: &Vec<&[u8]>) -> Vec<Vec<f64>> {
     let params = set_params(units);
 
     let seq_num = sequences.len();
-    let mut scores = vec![vec![0; seq_num]; seq_num];
+    let mut scores = vec![vec![0.0; seq_num]; seq_num];
 
     for i in 0..seq_num {
         for j in i + 1..seq_num {
@@ -129,8 +136,8 @@ mod tests {
     #[test]
     fn eddc_heuristic_test() {
         let sequences = vec![vec![0, 0, 0, 0, 0], vec![1, 1, 1, 1, 1]];
-        let units = vec![vec!['A', 'T'], vec!['A', 'A', 'T']];
-        let correct_scores = vec![vec![0, 10], vec![10, 0]];
+        let units = vec![b"AT".as_slice(), b"AAT".as_slice()];
+        let correct_scores = vec![vec![0.0, 5.0], vec![5.0, 0.0]];
         let scores = eddc_heuristic(&sequences, &units);
         assert_eq!(correct_scores, scores);
     }
--- eddc-heuristics/src/main.rs
+++ eddc-heuristics/src/main.rs
@@ -1,3 +1,57 @@
+mod lib;
+mod string_decomposer;
+use std::{io, vec};
+
 fn main() {
-    println!("Hello, world!");
+    let mut reads = vec![];
+    let mut units = vec![];
+    {
+        loop {
+            let mut seq_summary = String::new();
+            let mut seq = String::new();
+            match io::stdin().read_line(&mut seq_summary) {
+                Ok(_) => match io::stdin().read_line(&mut seq) {
+                    Ok(length) => {
+                        if length == 0 {
+                            break;
+                        }
+                        seq.retain(|c| c != '\n');
+                        if seq_summary.into_bytes()[2] == '(' as u8 {
+                            reads.push(seq);
+                        } else {
+                            units.push(seq);
+                        }
+                    }
+                    Err(error) => println!("error: {error}"),
+                },
+                Err(_) => {
+                    break;
+                }
+            }
+        }
+    }
+    // println!("{:?}", units);
+
+    let mut encoded_reads = vec![vec![]; reads.len()];
+
+    let reads_u8: Vec<&[u8]> = reads.iter().map(|seq| (&*seq).as_bytes()).collect();
+    let units_u8: Vec<&[u8]> = units.iter().map(|seq| (&*seq).as_bytes()).collect();
+    let params = string_decomposer::lib::StringDecompParameters::new(1, -1, -1, -1);
+
+    for (i, &read) in reads_u8.iter().enumerate() {
+        let decomposed = string_decomposer::lib::DecomposedSeq::new(read, &units_u8, &params);
+
+        for e in decomposed.encoding() {
+            encoded_reads[i].push(e.id());
+        }
+        // println!("{:?}", decomposed);
+    }
+
+    let score_matrix = lib::eddc_heuristic(&encoded_reads, &units_u8);
+    for v in &score_matrix {
+        for &x in v {
+            print!("{} ", x);
+        }
+        println!();
+    }
 }
