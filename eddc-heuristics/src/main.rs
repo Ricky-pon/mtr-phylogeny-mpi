c5def6cba8cc69535012d5dc3e255f49086b806e
Ricky-pon <ricky.quiken@gmail.com>
2022-09-13 15:30:49 +0200

    fix fasta input

--- eddc-heuristics/src/main.rs
+++ eddc-heuristics/src/main.rs
@@ -1,56 +1,40 @@
+mod fasta;
 mod lib;
-use std::{io, vec};
+use std::vec;
 use string_decomposer;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let mut reads = vec![];
     let mut units = vec![];
-    {
-        loop {
-            let mut seq_summary = String::new();
-            let mut seq = String::new();
-            match io::stdin().read_line(&mut seq_summary) {
-                Ok(_) => match io::stdin().read_line(&mut seq) {
-                    Ok(length) => {
-                        if length == 0 {
-                            break;
-                        }
-                        seq.retain(|c| c != '\n');
-                        if seq_summary.into_bytes()[2] == '(' as u8 {
-                            reads.push(seq);
-                        } else {
-                            units.push(seq);
-                        }
-                    }
-                    Err(error) => println!("error: {error}"),
-                },
-                Err(_) => {
-                    break;
-                }
-            }
+
+    let args: Vec<_> = std::env::args().collect();
+    let records = fasta::parse_into_vec(&args[1])?;
+    for record in records.iter() {
+        if record.id().as_bytes()[2] == '(' as u8 {
+            reads.push(record.seq());
+        } else {
+            units.push(record.seq());
         }
     }
 
     let mut encoded_reads = vec![vec![]; reads.len()];
 
-    let reads_u8: Vec<&[u8]> = reads.iter().map(|seq| (&*seq).as_bytes()).collect();
-    let units_u8: Vec<&[u8]> = units.iter().map(|seq| (&*seq).as_bytes()).collect();
     let params = string_decomposer::StringDecompParameters::new(1, -1, -1, -1);
 
-    for (i, &read) in reads_u8.iter().enumerate() {
-        let decomposed = string_decomposer::DecomposedSeq::new(read, &units_u8, &params);
+    for (i, &read) in reads.iter().enumerate() {
+        let decomposed = string_decomposer::DecomposedSeq::new(read, &units, &params);
 
         for e in decomposed.encoding() {
             encoded_reads[i].push(e.id());
         }
-        // println!("{:?}", decomposed);
     }
 
-    let score_matrix = lib::eddc_heuristic(&encoded_reads, &units_u8);
+    let score_matrix = lib::eddc_heuristic(&encoded_reads, &units);
     for v in &score_matrix {
         for &x in v {
             print!("{} ", x);
         }
         println!();
     }
+    Ok(())
 }
